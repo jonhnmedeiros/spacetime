eee1338a454d5cbcf11bde25aa2f2b63fdd257d5
Jonathan Medeiros <jonathan.medeiros@serpro.gov.br>
2023-05-18 12:33:44 -0300

    Aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 // HTTP Method: GET, POST, PUT, PATCH, DELETE
@@ -9,11 +13,18 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 listening on port 3333')
