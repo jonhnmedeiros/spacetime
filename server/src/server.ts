8d60ff11a181569a566e9f9de4234298c3405379
Jonathan Medeiros <jonathan.medeiros@serpro.gov.br>
2023-05-18 06:06:22 -0300

    Aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,15 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
-
 // HTTP Method: GET, POST, PUT, PATCH, DELETE
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-  return users
-})
-
 // API RESTful
+app.register(cors, {
+  origin: true,
+})
+app.register(memoriesRoutes)
 
 app
   .listen({
